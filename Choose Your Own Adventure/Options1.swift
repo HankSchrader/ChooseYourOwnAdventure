b66c816fbe8ba046b88c606c554c720a23bcd5a1
emikac <emikac@indiana.edu>
2017-06-23 18:50:42 -0400

    created second view controller

---
+++ Choose Your Own Adventure/Options1.swift
@@ -0,0 +1,35 @@
+//
+//  Options1.swift
+//  Choose Your Own Adventure
+//
+//  Created by Erik Mikac on 6/22/17.
+//  Copyright © 2017 Erik Mikac. All rights reserved.
+//
+
+import UIKit
+
+class Options1: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
